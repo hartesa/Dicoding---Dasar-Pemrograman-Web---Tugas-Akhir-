8223ba9fd0e1a52e3bb2abda264244e8475f54fd
hartesa <106516372+hartesa@users.noreply.github.com>
2022-10-16 12:56:51 +0700

    Revising JS function for the buttons

--- src/script.js
+++ src/script.js
@@ -8,7 +8,7 @@ document
   .addEventListener("click", function () {
     document.getElementById("img1").src = "src/1b.jpeg";
     document.getElementById(
-      "content1"
+      "desc"
     ).innerHTML = `Xuanzang Sanzang (onyomi: Genjō Sanzō) is one of the honorific titles given to a monk known as Chen Hui. He is best known for traveling to India and influencing the acceptance of Buddhism into China. His adventures were romanticized in one of the Four Classics of Chinese Literature, Journey to the West. Due to the vague nature of Sanzang's gender within the original Japanese rendition of the story, it's not uncommon for a female Sanzang to appear in Japanese created media.
 
 The monk's Warriors Orochi counterpart is known as a woman who is called Sanzang in English and the full title of Sanzang Fashi (三蔵法師, onyomi: Sanzō Hōshi) in Japanese. The previous style of English localization called her "San Zang" during her debut. Her character's name now reflects the pinyin spelling of her name as Tripitaka in some forms of media.
@@ -20,7 +20,7 @@ document
   .addEventListener("click", function () {
     document.getElementById("img1").src = "src/2.jpeg";
     document.getElementById(
-      "content1"
+      "desc"
     ).innerHTML = `Sun Wukong (onyomi: Son Gokū) is one of Sanzang's famous traveling disciples in Journey to the West. Originally born from a stone atop a mountain and having studied under Subhuti who gave him his current name, Wukong prided himself on his immortality, strength, and stature as the Handsome Monkey King. After various accounts that caused animosity between him and the Heavenly Palace, he was imprisoned for 500 years and then later forced to protect Sanzang on the way to India and back to atone for his past grievances. He is popularly known for using his mischievousness, optimism, and his wily cleverness to protect his master while developing a closer relationship to both him and his fellow disciples in spite of his abrasive attitude.
 
 His Warriors Orochi incarnation can be unlocked during his debut by clearing Wei's scenario. Players can unlock him in Warriors Orochi 3 by viewing all of the game's endings.
@@ -32,7 +32,7 @@ document
   .addEventListener("click", function () {
     document.getElementById("img1").src = "src/3.jpeg";
     document.getElementById(
-      "content1"
+      "desc"
     ).innerHTML = `Zhu Bajie (猪八戒, onyomi: Cho Hakkai) is one of Sanzang's famous disciples from Journey to the West. Originally known as the Marshall Tianpeng (天蓬元帥) during his time in heaven, an eloping scandal with a fellow female deity (stated to be Chang'e in some adaptations) caused him to be punished by heaven via being reincarnated into a human; however, he somehow in varying accounts, ended up in a female pig's womb instead, making him a pig demon who retained all of his memories from his time in heaven.
 
 After another incident depending on the adaptation, the end result is that he eventually is requested by Guanyin to join up with Tang Sanzang and Sun Wukong as their second-to-third new member to their journey west while also being the strongest behind the latter. Despite his perversion towards women, lazy nature and his notable hunger, Bajie tends to often be a bit positive on his outlook while also providing comic relief.
@@ -44,7 +44,7 @@ document
   .addEventListener("click", function () {
     document.getElementById("img1").src = "src/5.jpeg";
     document.getElementById(
-      "content1"
+      "desc"
     ).innerHTML = `Zhu Liangling (朱涼鈴, pinyin: Zhū Liánglíng, onyomi: Shū Ryōrin) is the third party member that Sanzo meets in Saiyuki: Journey West. Although her name and origin are fictional, she seems to take the position of one of Xuanzang's companions from the novel, Yu Long. Better known as Xuanzang's white horse, Yu Long was the son of the dragon king, King Ao Kuang. At one point in the novel, he morphed into the form of a beautiful maiden and fought with a sword.`;
   });
 document
@@ -52,7 +52,7 @@ document
   .addEventListener("click", function () {
     document.getElementById("img1").src = "src/4.jpeg";
     document.getElementById(
-      "content1"
+      "desc"
     ).innerHTML = `Sha Wujing (沙悟浄, onyomi: Sha Gojō) is one of Sanzo's famous companions in Journey to the West. He is the fourth party member to join Sanzo in Saiyuki: Journey West.`;
   });
 document
@@ -60,6 +60,6 @@ document
   .addEventListener("click", function () {
     document.getElementById("img1").src = "src/6.jpeg";
     document.getElementById(
-      "content1"
+      "desc"
     ).innerHTML = `Lady Jiehua (桔花公主, pinyin: Jiéhuā Gōngzhǔ, onyomi: Kikka Kōshu; lit: Princess Jiéhuā) is an original character made for Saiyuki: Journey West. She is the second female member to join Sanzo and the last recruitable character in the game's first chapter.`;
   });
